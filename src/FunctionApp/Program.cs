a9704a4eb94bca5c09cb1216273f7c423820fab4
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2023-07-12 17:09:24 +1000

    change to isolated function app

---
+++ src/FunctionApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSamples.FunctionApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                //.ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services =>
+                {
+                    //services.AddDbContext(options
+                    //  => Options.UseSqlServer(
+                    //         Environment.GetEnvironmentVariable("MyConnection")));
+                    //services.AddOptions()
+                    //    .Configure((settings, configuration)
+                    //         => configuration.GetSection("MyOptions").Bind(settings));
+                    //services.AddScoped();
+                }).Build();
+            host.Run();
+        }
+    }
+}
