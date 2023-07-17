aa54ae3a12ab1b96084e680d1003fdc51f1355f3
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2023-07-17 10:10:05 +1000

    temp changes

--- utilities/staticValidation/helper/helper.psm1
+++ utilities/staticValidation/helper/helper.psm1
@@ -3,7 +3,7 @@
 ##############################
 $repoRootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.Parent.FullName
 
-. (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-NestedResourceList.ps1')
+# . (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-NestedResourceList.ps1')
 . (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-ScopeOfTemplateFile.ps1')
 . (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-ModuleTestFileList.ps1')
 . (Join-Path $repoRootPath 'utilities' 'tools' 'Get-CrossReferencedModuleList.ps1')
