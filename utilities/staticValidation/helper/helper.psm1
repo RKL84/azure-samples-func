5e750244991a2c8e0588da02db9907bcbeaf22b3
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2023-07-17 10:40:53 +1000

    temp changes

--- utilities/staticValidation/helper/helper.psm1
+++ utilities/staticValidation/helper/helper.psm1
@@ -1,7 +1,7 @@
 ﻿##############################
 #   Load general functions   #
 ##############################
-$repoRootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.Parent.FullName
+$repoRootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
 
 . (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-ScopeOfTemplateFile.ps1')
 . (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-ModuleTestFileList.ps1')
