1cfe40d374d94ba066c06476a3aa529bfcc156dd
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2023-07-17 11:52:35 +1000

    upadte pipeline

--- utilities/staticValidation/helper/helper.psm1
+++ utilities/staticValidation/helper/helper.psm1
@@ -4,4 +4,3 @@
 $repoRootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
 
 . (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-ScopeOfTemplateFile.ps1')
-. (Join-Path $repoRootPath 'utilities' 'scripts' 'Get-ModuleTestFileList.ps1')
